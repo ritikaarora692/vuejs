480278941fb5ddfed60072464b1f24b97cda2319
Ritika Arora <ritika.arora01@nagarro.com>
2018-11-13 13:01:17 +0530

    First Commit

---
+++ src/components/BandItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <li @click="toggle()">
+            {{band.name}}
+        <p v-show="open">
+            {{band.songs | join}}
+            <input type="text" placeholder="Add more songs..." @click.stop @keyup.enter="addSong" v-model="newSong"> 
+        </p>
+
+    </li>
+
+</template>
+
+<script>
+export default{
+    name: 'BandItem',
+    data(){
+        return{
+            newSong: '',
+            open: false
+        }
+    },
+    props:{
+        band: Object
+    },
+    methods:
+    {
+        toggle(){
+            this.open=!this.open;
+        },
+        addSong () {
+            this.band.songs.push(this.newSong)
+            this.newSong = ''
+        }
+    },
+    filters:{
+        join(arr){
+            return arr.join(', ')
+        }
+    }
+}
+</script>
---
+++ src/components/BandsList.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+<input type="text" placeholder="Add Band"  @click.stop v-model="newBand">
+<button @click="addBand()"> Add </button>
+    <ul>
+        <BandItem v-for="band in bands" :key="band.id" :band=band>
+            {{band.name}}
+        </BandItem>
+    </ul>
+    </div>
+</template>
+<script>
+import BandItem from './BandItem.vue'
+export default{
+    name: 'BandList',
+    components: {
+    BandItem
+  },
+    data(){
+        return{
+            newBand: '',
+            bands:[{
+                id: 1,
+                name: 'ColdPlay',
+                songs: ['Hymn For the Weekend', 'Paradise']
+            },{
+                id: 2,
+                name: 'Chainsmoker',
+                songs: ['closer', 'Something just like this']
+            }]
+        }
+    },
+    methods:{
+        addBand(){
+            this.bands.push({
+                id: Math.floor((Math.random()*100)+1),
+                name: this.newBand,
+                songs: []
+            })
+            this.newBand = ''
+        }
+    }
+}
+</script>
